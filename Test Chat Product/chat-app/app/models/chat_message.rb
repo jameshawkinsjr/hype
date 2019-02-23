a3e487cd2bac40a88d1b64bcbe7d5feec662da77
James <james@jameshawkins.com>
2019-02-23 12:11:22 -0800

    Add project skeleton

--- Test Chat Product/chat-app/app/models/chat_message.rb
+++
@@ -1,6 +0,0 @@
-class ChatMessage < ApplicationRecord
-    after_create_commit do
-        ChatMessageCreationEventBroadcastJob.perform_later(self)
-    end
-
-end
--- Test Chat Product/chat-app/db/migrate/20190222054107_create_chat_messages.rb
+++
@@ -1,9 +0,0 @@
-class CreateChatMessages < ActiveRecord::Migration[5.2]
-  def change
-    create_table :chat_messages do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
