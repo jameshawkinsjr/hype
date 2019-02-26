8da1e36ec3d796f438cecbbb3328c546b3816292
James <james@jameshawkins.com>
2019-02-25 21:26:03 -0800

    Update Heroku config files

--- frontend/hype.jsx
+++ frontend/hype.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Root from './components/root.jsx';
 import * as ApiUtil from './util/session_api_util';
 import configureStore from './store/store';
-import { login } from './actions/session_actions';
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
