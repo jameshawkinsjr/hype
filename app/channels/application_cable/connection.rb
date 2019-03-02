924e50aad249e03d9fac107b01e6d23951cf5d1f
James <james@jameshawkins.com>
2019-03-02 13:30:18 -0800

    Add ActionCable verified user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,18 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      self.current_user = find_current_user
     end
 
-    private
-      def find_verified_user
-        if verified_user = User.find_by(id: env['warden'].user.id)
-          verified_user
-        else
-          reject_unauthorized_connection
-        end
+    protected
+    def find_current_user
+      current_user = User.find_by(session_token: env['rack.session'][:session_token])
+      if current_user
+        current_user
+      else
+        reject_unauthorized_connection
       end
+    end
   end
 end
